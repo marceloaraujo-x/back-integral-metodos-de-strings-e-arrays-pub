323f5df7b214c72c20fb18c81025d5f6136cea78
Marcelo Araújo <marcelo.niklaus@gmail.com>
2021-06-30 17:07:23 -0300

    Exercício resolvido - Programa que valida CPF e CNPJ

--- classe-02/index.js
+++ classe-02/index.js
@@ -1,2 +1,25 @@
 const cpf = "12345678900";
-const cnpj = "12345678900";
+const cnpj = "12345678000199";
+
+if (cpf.length === 11) {
+    let cpfFormatado = cpf.split("");
+
+    cpfFormatado.splice(3, 0, ".");
+    cpfFormatado.splice(7, 0, ".");
+    cpfFormatado.splice(11, 0, "-");
+    console.log(cpfFormatado.join(""));
+} else {
+    console.log("CPF Inválido");
+}
+if (cnpj.length === 14) {
+    let cnpjFormatado = cnpj.split("");
+
+    cnpjFormatado.splice(2, 0, ".");
+    cnpjFormatado.splice(6, 0, ".");
+    cnpjFormatado.splice(10, 0, "/");
+    cnpjFormatado.splice(15, 0, "-");
+    console.log(cnpjFormatado.join(""));
+
+} else {
+    console.log("CNPJ Inválido");
+}
