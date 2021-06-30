5b562d5f418cccc6f43a18e1c6ade31434d7b23e
Marcelo Araújo <marcelo.niklaus@gmail.com>
2021-06-30 15:44:38 -0300

    Exercício resolvido que verifica se tem palavras proibidas

--- classe-01/index.js
+++ classe-01/index.js
@@ -1,1 +1,7 @@
 const comentario = "Esse COVID é muito perigoso!";
+
+if (comentario.includes("COVID") || comentario.includes("covid") || comentario.includes("PANDEMIA") || comentario.includes("pandemia")) {
+    console.log("Comentário bloqueado por conter palavras proibidas");
+} else {
+    console.log("Comentário autorizado");
+}
