4a32163e638a23d33c8f24334bd5935305a70832
Marcelo Araújo <marcelo.niklaus@gmail.com>
2021-07-01 10:48:35 -0300

    Resolução exercício classe-06

--- classe-06/index.js
+++ classe-06/index.js
@@ -1,1 +1,10 @@
 const frutas = ['Banana', 'Maçã', 'Abacaxi', 'Pêra', 'Uva'];
+
+frutas.reverse();
+const novoString = frutas.join(", ");
+console.log(novoString);
+
+frutas.reverse();
+frutas.splice(0, 1);
+frutas.splice(frutas.length - 1, 1, "Melão");
+console.log(frutas);
