5083dbe9ee3a3cc673837006b7f3d8bc775a2618
Marcelo Araújo <marcelo.niklaus@gmail.com>
2021-07-01 10:26:58 -0300

    Resolução - Encontrar posição do livro na estante

--- classe-05/index.js
+++ classe-05/index.js
@@ -1,2 +1,4 @@
 const livros = ['Guerra e Paz', 'A Montanha Mágica', 'Cem Anos de Solidão', 'Dom Quixote', 'A Divina Comédia'];
 const nomeDoLivro = "Dom Quixote";
+
+console.log(livros.indexOf(nomeDoLivro) + 1);
