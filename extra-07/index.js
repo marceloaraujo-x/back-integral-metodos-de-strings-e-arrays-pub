4ffe18ac0d6a175a5e3a7272ad6ac38d3e7b3ffb
Marcelo Araújo <marcelo.niklaus@gmail.com>
2022-04-07 01:04:12 -0300

    Exercício extra 07 resolvido

--- extra-07/index.js
+++ extra-07/index.js
@@ -1,1 +1,18 @@
-const celular = 7199918888;
+const celular = 99918888;
+
+const celularString = `${celular}`;
+
+if (celularString.length === 8) {
+    console.log(`9 ${celularString.substr(0, 4)}-${celularString.substr(4)}`);
+} else if (celularString.length === 9) {
+    console.log(`${celularString.substr(0, 1)} ${celularString.substr(1, 4)}-${celularString.substr(5)}`)
+} else if (celularString.length === 10) {
+    console.log(`(${celularString.substr(0, 2)}) 9 ${celularString.substr(2, 4)}-${celularString.substr(6)}`);
+} else if (celularString.length === 11) {
+    console.log(`(${celularString.substr(0, 2)}) ${celularString.substr(2, 1)} ${celularString.substr(3, 4)}-${celularString.substr(7)}`)
+} else {
+    console.log("Número de celular inválido");
+}
+
+
+
