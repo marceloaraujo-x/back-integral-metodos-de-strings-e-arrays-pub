0d7783bd44c8fce219f2e8038d24718d0b83acaa
Marcelo Araújo <marcelo.niklaus@gmail.com>
2022-04-08 16:07:29 -0300

    Exercício extra 08 resolvido

--- extra-08/index.js
+++ extra-08/index.js
@@ -1,1 +1,13 @@
 const email = "aluno@cubos.academy";
+
+function validarEmail(email) {
+    if (email.includes("@")) {
+        if (email.includes(".")) {
+            if (email.indexOf(".") !== 0 && email.lastIndexOf(".") !== email.length - 1) {
+                console.log("O email é valido");
+            } else { console.log("Email inválido! O email não pode iniciar nem terminar com ponto") }
+        } else { console.log("Email inválido! É necessário haver um ponto '.' no email") }
+    } else { console.log("Email inválido! É necessário ter '@' no email") };
+}
+
+validarEmail(email)
